17177f39967facd3ec2abb0a57476eb2de1b8bc5
三島明恵 <myunghye.kor@gmail.com>
2018-11-17 07:50:48 +0900

    first commit

---
+++ components/AddBtn.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="add" @click="$emit('add')">+</div>
+</template>
+
+<style scoped>
+.add {
+  position: absolute;
+  bottom: 15px;
+  right: 15px;
+  border-radius: 50%;
+  background: #fff;
+  width: 50px;
+  height: 50px;
+  font-size: 40px;
+  line-height: 50px;
+  cursor: pointer;
+  text-align: center;
+}
+</style>
